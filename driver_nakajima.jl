c7479d705c8bbff0d509eaa61683cfe9c11f8695
Yakov <jakovbraver@gmail.com>
2022-05-08 15:07:41 +0300

    Add wavefunctions plotting

--- driver_nakajima.jl
+++ driver_nakajima.jl
@@ -2,11 +2,14 @@ using Plots, LaTeXStrings
 
 include("bandsolvers.jl")
 
-phases = range(0, π, length=61) # values of the adiabatic phase in (S32)
+# Energy spectrum
+
+phases = range(0, π, length=61)
 n_cells = 5
 n_min = 1
 n_max = 5
-e, E = compute_floquet_bands_with_boundary(;n=n_cells, n_min, n_max, phases, s=2, gₗ=-20, Vₗ=-30, λₗ=0, λₛ=0, ω=0, pumptype=:space)
+gₗ = -20; Vₗ = -30
+e, E = compute_floquet_bands_with_boundary(;n=n_cells, n_min, n_max, phases, s=2, gₗ, Vₗ, λₗ=0, λₛ=0, ω=0, pumptype=:space)
 
 fig = plot();
 for r in eachrow(e)
@@ -15,6 +18,29 @@ end
 plot!(xlabel=L"\phi", ylabel="Energy", title=L"(V_S, V_L) = (20, 30)")
 ylims!(-Inf, 0)
 savefig("nakajima-spectrum.pdf")
+
+# Wavefunctions
+
+"Reconstruct the coordinate space wavefunction 𝜓(𝑥) = ∑ⱼ𝑐ⱼsin(𝑗𝑥/𝑛) / √(𝑛π/2)"
+function make_coordinate_state(x::AbstractVector{<:Real}, coeffs::AbstractVector{<:Number}; n)
+    ψ = zeros(eltype(coeffs), length(x))
+    for (j, c) in enumerate(coeffs)
+        @. ψ += c * sin(j/n * x)
+    end
+    return ψ ./ sqrt(n*π/2)
+end
+
+ϕ = 3pi/4; ϕ_str = L"\phi = 3\pi/4"
+ee, EE, c, b = compute_floquet_bands_states(;n=n_cells, n_min, n_max, phases=[ϕ], s=2, gₗ, Vₗ, λₗ=0, λₛ=0, ω=0, pumptype=:space)
+
+x = range(0, n_cells*π, length=2001)
+U = @. gₗ*cos(2x)^2 + Vₗ*cos(x + ϕ)^2
+i = 5 # state number
+u = 4make_coordinate_state(x, c[:, i], n=n_cells) .+ ee[i]
+fig = plot(x ./ π, U, label=false, c=:white, lw=1)
+hline!([ee[i]], c=:white, ls=:dot, lw=0.5, label=false)
+plot!(x ./ π, u, label=false, title=ϕ_str, xlabel="z", ylabel="Energy")
+savefig("wf-phi-3pi4.pdf")
 
 ### Wannier centres
 
@@ -27,8 +53,8 @@ pos_lower, pos_higher, ε_lower, ε_higher = compute_wannier_centres(;N=n_cells,
 
 fig = plot();
 for (i, ϕ) in enumerate(phases)
-    scatter!(pos_lower[i], fill(ϕ, length(pos_lower[i])); marker_z=ε_lower[i],     c=:coolwarm, label=false, markerstrokewidth=0)
-    scatter!(pos_higher[i], fill(ϕ, length(pos_higher[i])); marker_z=ε_higher[i],  c=:coolwarm, label=false, markerstrokewidth=0)
+    scatter!(pos_lower[i],  fill(ϕ, length(pos_lower[i]));  marker_z=ε_lower[i],  c=:coolwarm, label=false, markerstrokewidth=0)
+    scatter!(pos_higher[i], fill(ϕ, length(pos_higher[i])); marker_z=ε_higher[i], c=:coolwarm, label=false, markerstrokewidth=0)
 end
 plot!(minorgrid=true, xlabel=L"z", ylabel=L"\phi", cbtitle="Energy", title=L"(V_S, V_L) = (20, 30)")
 savefig("nakajima-wannier.pdf")
