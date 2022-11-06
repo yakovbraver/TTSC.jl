7f7f6fb8f5cad7aca8d1ccc4083e1b0a68c5c335
Yakov <jakovbraver@gmail.com>
2022-11-06 14:17:05 +0200

    Add calculation of Wanniers

--- DeltaModel.jl
+++ DeltaModel.jl
@@ -1,7 +1,17 @@
 module DeltaModel
 
 import Roots
-using LinearAlgebra: eigen, schur, ⋅, diagm, diagind, eigvals, det
+using LinearAlgebra: eigen, schur, ⋅, svd
+
+"A type for storing the Wannier functions."
+mutable struct Wanniers
+    E::Matrix{Float64} # `E[j, i]` = mean energy of `j`th wannier at `i`th phase
+    pos::Matrix{Float64} # `pos[j, i]` = position eigenvalue of `j`th wannier at `i`th phase
+    d::Array{ComplexF64, 3} # `d[:, :, i]` = position eigenvectors at `i`th phase; see methods of `compute_wanniers!` for details
+end
+
+"Default-construct an empty `Wanniers` object."
+Wanniers() = Wanniers(Float64[;;], Float64[;;], ComplexF64[;;;])
 
 """
 A type representing the unperturbed Hamiltonian
@@ -16,6 +26,7 @@ mutable struct UnperturbedHamiltonian
     φₓ::Vector{Float64}
     E::Matrix{Float64}      # `E[i, j]` = `i`th eigenvalue of the band of interest at `j`th phase, `i` ∈ [1, `N`], `j` ∈ [1, `length(φₓ)`]
     c::Array{ComplexF64, 3} # `c[:, i, j]` = `i`th eigenvector of the band of interest at `j`th phase
+    w::Wanniers
 end
 
 """
@@ -24,8 +35,9 @@ Construct an `UnperturbedHamiltonian` object.
 function UnperturbedHamiltonian(n_cells::Integer; a::Real, λ::Real, U::Real, isperiodic::Bool, φₓ::AbstractVector{<:Real})
     E = Matrix{Float64}(undef, n_cells, length(φₓ))
     c = Array{ComplexF64,3}(undef, 6, n_cells, length(φₓ))
-
-    UnperturbedHamiltonian(Int(n_cells), Float64(a), Float64(λ), Float64(U), isperiodic, collect(Float64, φₓ), E, c)
+    w = Wanniers(Matrix{Float64}(undef, n_cells, length(φₓ)), Matrix{Float64}(undef, n_cells, length(φₓ)),
+                 Array{ComplexF64,3}(undef, n_cells, n_cells, length(φₓ)))
+    UnperturbedHamiltonian(Int(n_cells), Float64(a), Float64(λ), Float64(U), isperiodic, collect(Float64, φₓ), E, c, w)
 end
 
 "Return 𝑔ₙ(𝑥) which realises the pumping protocol."
@@ -33,7 +45,7 @@ function 𝑔(x; n, a)
     Int( n/3 <= (x % a)/a < (n+1)/3 )
 end
 
-"Return cos(𝑘𝑎)/2𝜅₁𝜅₂𝜅₃ for a given energy `ε` and phase `φ`."
+"Return cos(𝑘𝑎) for a given energy `ε` and phase `φ`."
 function cos_ka(ε::Real; φ::Real, uh::UnperturbedHamiltonian)
     (;a, U, λ) = uh
     κ = [sqrt(ε - U*cos(φ + 2π*n/3)) for n = 0:2]
@@ -45,6 +57,7 @@ function cos_ka(ε::Real; φ::Real, uh::UnperturbedHamiltonian)
     ) / 2κ[1]κ[2]κ[3]
 end
 
+"Return the 6×6 matrix that characterises the system."
 function system_matrix(ε::Real; φ::Real, ka::Real, uh::UnperturbedHamiltonian)
     (;a, U, λ) = uh
     κ = [sqrt(ε - U*cos(φ + 2π*n/3)) for n = 0:2]
@@ -67,21 +80,20 @@ function diagonalise!(uh::UnperturbedHamiltonian, bounds::Tuple{<:Real, <:Real})
             ka = 2π*(i-1)/N
             # eigenenergies
             if i <= N÷2 + 1
-                E[i, j] = Roots.find_zero(ε -> cos_ka(ε; φ, uh) - cos(ka), bounds, Roots.A42(), rtol=1e-5)
+                E[i, j] = Roots.find_zero(ε -> cos_ka(ε; φ, uh) - cos(ka), bounds, Roots.A42(), rtol=1e-8)
             else
                 E[i, j] = E[N-i+2, j]
             end
             # eigenfunctions
-            for m = 1:6
-                M = system_matrix(E[i, j]; φ, ka, uh)
-                uh.c[m, i, j] = (iseven(m) ? 1 : -1) * det(M[2:6, [1:m-1; m+1:6]])
-            end
+            M = system_matrix(E[i, j]; φ, ka, uh)
+            uh.c[:, i, j] = svd(M).V[:, end]
+            
             # normalise coefficients
             κ = [sqrt(E[i, j] - U*cos(φ + 2π*n/3)) for n = 0:2]
             c = view(uh.c, :, i, j)
-            X = (c⋅c)a/6 + (c[2]^2 - c[1]^2)sin(2κ[1]a/3)/4κ[1] + (1-cos(2κ[1]a/3))c[1]c[2]/2κ[1] +
-                sin(κ[2]a/3)/κ[2] * (c[3]c[4]sin(κ[2]a) + (c[4]^2 - c[3]^2)*cos(κ[2]a)/2) +
-                sin(κ[3]a/3)/κ[3] * (c[5]c[6]sin(5κ[3]a/3) + (c[6]^2 - c[5]^2)*cos(5κ[3]a/3)/2)
+            X = (c⋅c)a/6 + real(c[1]c[2]')sin(κ[1]a/3)^2/κ[1] + (abs2(c[2]) - abs2(c[1]))sin(2κ[1]a/3)/4κ[1] +
+                sin(κ[2]a/3)/κ[2] * (real(c[3]c[4]')sin(κ[2]a) + (abs2(c[4]) - abs2(c[3]))cos(κ[2]a)/2) +
+                sin(κ[3]a/3)/κ[3] * (real(c[5]c[6]')sin(5κ[3]a/3) + (abs2(c[6]) - abs2(c[5]))cos(5κ[3]a/3)/2)
             c ./= √(N*X) 
         end
     end
@@ -105,13 +117,68 @@ function make_eigenfunctions(uh::UnperturbedHamiltonian, n_x::Integer, whichphas
                 @. ψ[mask, j, i] = c[2n-1, j, iϕ]sin(κ[n]x[mask]) + c[2n, j, iϕ]cos(κ[n]x[mask])
             end
             # repeat the first cell according to the Bloch's theorem
-            for n in 2:N
-                @. ψ[3n_x*(n-1)+1:3n_x*n, j, i] = ψ[3n_x*(n-2)+1:3n_x*(n-1), j, i] * cis(2π*j/(N*a))
+            for n in 1:N-1
+                @. ψ[3n_x*n+1:3n_x*(n+1), j, i] = ψ[1:3n_x, j, i] * cis(2π*(j-1)n/N)
             end
             ψ[end, j, i] = ψ[1, j, i] # close the loop vor visual convenience
         end
     end
     return x, ψ
+end
+
+"Calculate Wannier vectors for the unperturbed Hamiltonian `uh`."
+function compute_wanniers!(uh::UnperturbedHamiltonian)
+    (;N, a, φₓ, U, c, E) = uh
+
+    X = Matrix{ComplexF64}(undef, N, N) # position operator
+    
+    𝜅(i, j, iϕ) = √(E[j, iϕ] - U*cos(φₓ[iϕ] + 2π*(i-1)/3))
+    k₂ = 2π/(N*a)
+    𝐹(x, i, n, j′, j, iϕ) = begin
+        κʲ = 𝜅(i, j, iϕ)
+        κʲ′ = 𝜅(i, j′, iϕ)
+        cis((n-1)*2π*(j-j′)/N - (κʲ′ + κʲ - k₂)x) / 4 * (
+            im * (c[2i-1, j′, iϕ] + im*c[2i, j′, iϕ])' * (c[2i-1, j, iϕ] - im*c[2i, j, iϕ]) / (κʲ′ + κʲ - k₂) +
+            (c[2i-1, j, iϕ] + im*c[2i, j, iϕ]) * cis(2κʲ*x) * ( 
+                (c[2i, j′, iϕ] - im*c[2i-1, j′, iϕ]) / (-κʲ′ + κʲ + k₂) +
+                (c[2i, j′, iϕ] + im*c[2i-1, j′, iϕ]) / ( κʲ′ + κʲ + k₂) * cis(-2κʲ′*x) )' +
+            (c[2i-1, j′, iϕ] - im*c[2i, j′, iϕ])' * (c[2i, j, iϕ] + im*c[2i-1, j, iϕ]) * cis(2κʲ′*x) / (κʲ′ - κʲ + k₂) )
+    end
+
+    for iϕ in eachindex(φₓ)
+        for j in 1:N
+            for j′ in 1:N
+                X[j′, j] = 0
+                for n = 1:N, i = 1:3
+                    X[j′, j] += 𝐹((n-1)a + i*a/3, i, n, j′, j, iϕ) - 𝐹((n-1)a + (i-1)a/3, i, n, j′, j, iϕ)
+                end
+            end
+        end
+        # `eigen` does not guarantee orthogonality of eigenvectors in case of degeneracies for `X` unitary, so use `schur`
+        # (although a degeneracy of coordinates eigenvalues is unlikely here)
+        _, uh.w.d[:, :, iϕ], pos_complex = schur(X)
+        pos_real = @. (angle(pos_complex) + pi) / k₂ # shift angle from [-π, π) to [0, 2π)
+        sp = sortperm(pos_real)                         # sort the eigenvalues
+        uh.w.pos[:, iϕ] = pos_real[sp]
+        @views Base.permutecols!!(uh.w.d[:, :, iϕ], sp) # sort the eigenvectors in the same way
+        uh.w.E[:, iϕ] = [abs2.(dˣ) ⋅ E[:, iϕ] for dˣ in eachcol(uh.w.d[:, :, iϕ])]
+    end
+end
+
+"""
+Construct Wannier functions at each phase number in `whichphases`. `n_x` specifies the number of points to use for each site.
+All Wannier functions contained in `uh` are constructed. In the process, energy eigenfunctions are also constructed.
+Return `x, ψ, w`, where `ψ[:, j, i]` = `j`th eigenfunction at `i`th phase, and `w[:, j, i]` = `j`th Wannier function at `i`th phase.
+"""
+function make_wannierfunctions(uh::UnperturbedHamiltonian, n_x::Integer, whichphases::AbstractVector{<:Integer})
+    x, ψ = make_eigenfunctions(uh, n_x, whichphases)
+    w = similar(ψ)
+    for i in eachindex(whichphases)
+        for j in 1:uh.N
+            w[:, j, i] = sum(uh.w.d[k, j, i] * ψ[:, k, i] for k = 1:uh.N)
+        end
+    end
+    return x, ψ, w
 end
 
 end
--- driver_delta.jl
+++ driver_delta.jl
@@ -6,32 +6,46 @@ theme(:dark, size=(800, 500))
 includet("DeltaModel.jl")
 import .DeltaModel
 
-function plot_potential(uh::DeltaModel.UnperturbedHamiltonian)
-    (;a, U, N) = uh
+"Produce an animation of potential if `iϕ == nothing`; otherwise, plot the potential at phase number `iϕ`."
+function plot_potential(uh::DeltaModel.UnperturbedHamiltonian; U::Real, lift::Real=0, iϕ::Union{Nothing, <:Real}=nothing)
+    (;a, φₓ, N) = uh
     x = range(0, N * a, length=100)
     𝑈 = [x -> U * DeltaModel.𝑔(x; n, a) for n = 0:2]
     barriers = [-0.01, 0.01]
     for i = 1:3n_cells
         append!(barriers, [i*a/3 - 0.01, i*a/3 + 0.01])
     end
-    @gif for φ in uh.φₓ
+    if iϕ === nothing
+        @gif for φ in φₓ
+            V = zeros(length(x))
+            for n in 1:3
+                V .+= 𝑈[n].(x) .* cos(φ + 2π*(n-1)/3)
+            end
+            plot(x, V.+lift, ylims=(-1.1U, 2U).+lift, lw=2, c=:white, label=false, xlabel=L"x", ylabel="Energy",
+                title=L"U=%$U, a=%$a, \varphi=%$(round(φ, digits=3))", titlepos=:left)
+            vspan!(barriers, c=:grey, label=false)
+        end
+    else
+        φ = φₓ[iϕ]
         V = zeros(length(x))
         for n in 1:3
             V .+= 𝑈[n].(x) .* cos(φ + 2π*(n-1)/3)
         end
-        plot(x, V, ylims=(-1.1U, 2U), lw=2, label=false, xlabel=L"x", ylabel="Energy",
-             title=L"U=%$U, a=%$a, \varphi=%$(round(φ, digits=3))", titlepos=:left)
+        plot(x, V.+lift, ylims=(-1.1U, 2U).+lift, lw=2, c=:white, label=false, xlabel=L"x", ylabel="Energy",
+            title=L"U=%$U, a=%$a, \varphi=%$(round(φ, digits=3))", titlepos=:left)
         vspan!(barriers, c=:grey, label=false)
     end
 end
 
-n_cells = 3
+n_cells = 5
 a = 2; λ = 500; U = 3
 φₓ = range(0, 2π, length=61)
 h = DeltaModel.UnperturbedHamiltonian(n_cells; a, λ, U, isperiodic=true, φₓ)
 
-plot_potential(h, iϕ)
+plot_potential(h; U, lift=0)
 savefig("potential.pdf")
+
+# dispersion
 
 function plot_dispersion(ε::AbstractVector; φ::Real, uh::DeltaModel.UnperturbedHamiltonian)
     cos_kL = [DeltaModel.cos_ka(E; φ, uh) for E in ε]
@@ -44,6 +58,8 @@ end
 plot_dispersion(ε; φ=φₓ[6], uh=h)
 savefig("solution.html")
 
+# spectrum
+
 bandbounds = [(346, 348), (349, 353), (354, 356.5)]
 fig = plot()
 for (j, bounds) in enumerate(bandbounds)
@@ -55,7 +71,39 @@ for (j, bounds) in enumerate(bandbounds)
 end
 display(fig)
 
-x, ψ = DeltaModel.make_eigenfunctions(h, 50, [15])
-plot(x, abs2.(ψ[:, 1, 1]))
-plot(x, abs2.(ψ[:, 2, 1]))
-plot(x, abs2.(ψ[:, 3, 1]))
+# eigenfunctions
+
+DeltaModel.diagonalise!(h, (354, 356.5))
+iϕ = 5
+n_x = 50
+x, ψ = DeltaModel.make_eigenfunctions(h, n_x, [iϕ])
+fig = plot()
+for n in 1:n_cells
+    plot!(x, abs2.(ψ[:, n, 1]) .+ h.E[n, iϕ])
+end
+ylims!(h.E[1, iϕ]-5, h.E[1, iϕ]+5)
+
+# Wannier centres
+
+DeltaModel.compute_wanniers!(h)
+
+fig = plot();
+for (i, ϕ) in enumerate(φₓ)
+    scatter!(h.w.pos[:, i], fill(ϕ, size(h.w.pos, 1)); marker_z=h.w.E[:, i], c=:coolwarm, label=false, markerstrokewidth=0)
+end
+plot!(minorgrid=true, xlabel=L"x", ylabel=L"\varphi", cbtitle="Energy")
+
+# Wannier functions
+
+x, _, w = DeltaModel.make_wannierfunctions(h, n_x, 1:length(φₓ))
+lift = minimum(h.w.E) - 0.5
+lims = (lift-1, lift+4)
+p = Progress(length(φₓ), 1)
+@gif for iϕ in eachindex(φₓ)
+    fig = plot_potential(h; U=0.2, lift, iϕ)
+    scatter!(h.w.pos[:, iϕ], h.w.E[:, iϕ]; label=false, markerstrokewidth=0, ylims=lims, c=1:n_cells, markersize=5)
+    for j in 1:n_cells
+        plot!(x, abs2.(w[:, j, iϕ]) .+ h.w.E[j, iϕ], label=false, c=j)
+    end
+    next!(p)
+end
