c3b5806007fdd3a2d92a03bbc551ba621506104a
Yakov <jakovbraver@gmail.com>
2023-05-20 19:32:14 +0300

    Update plotting

--- examples/SineModel/spatial.jl
+++ examples/SineModel/spatial.jl
@@ -3,7 +3,7 @@ import TTSC.SineModel as sm
 using Plots, LaTeXStrings, ProgressMeter
 
 plotlyjs()
-theme(:dark, size=(800, 600))
+theme(:dark, size=(800, 500))
 
 using LinearAlgebra.BLAS: set_num_threads
 set_num_threads(1)
@@ -15,9 +15,9 @@ set_num_threads(1)
       range(3pi/4+0.1, pi, length=10)]
 n_cells = 4
 gₗ = -7640; Vₗ = -2
-# gₗ = -20; Vₗ = -30 corresponds exactly to the system in Nakajima et al. (https://www.nature.com/articles/nphys3622)
+# gₗ = -30; Vₗ = -20 # corresponds exactly to the system in Nakajima et al. (https://www.nature.com/articles/nphys3622)
 
-h = sm.UnperturbedHamiltonian(n_cells; M=1/2, gₗ, Vₗ, φₓ, maxband=30, isperiodic=true)
+h = sm.UnperturbedHamiltonian(n_cells; M=1/2, gₗ, Vₗ, φₓ, maxband=32, isperiodic=true)
 sm.diagonalise!(h)
 
 # Energy spectrum
@@ -40,7 +40,7 @@ end
 display(fig)
 
 # Wannier centres
-sm.compute_wanniers!(h, targetband=25, mixsubbands=false)
+sm.compute_wanniers!(h, targetband=27, mixsubbands=false)
 fig = plot();
 for (i, φ) in enumerate(φₓ)
     scatter!(h.w.pos[:, i], fill(φ, 2n_cells); marker_z=h.w.E[:, i], c=:coolwarm, label=false, markerstrokewidth=0)
@@ -48,17 +48,19 @@ end
 plot!(minorgrid=true, xlabel=L"x", ylabel=L"\varphi_x", cbtitle="Energy")
 
 # Wannier functions
-x = range(0, n_cells*π, length=50n_cells)
+x = range(0, n_cells*π, length=500n_cells)
 _, w = sm.make_wannierfunctions(h, x, 1:length(φₓ))
+x_U = range(0, n_cells*π, length=50n_cells)
 lims = (minimum(h.w.E)-0.5, maximum(h.w.E)+2)
 p = Progress(length(φₓ), 1)
 @gif for (i, φ) in enumerate(φₓ)
-    U = @. gₗ*cos(2x)^2 + Vₗ*cos(x + φ)^2
-    plot(x, U, label=false, ylims=lims)
-    scatter!(h.w.pos[:, i], h.w.E[:, i]; marker_z=h.w.E[:, i], c=:coolwarm, label=false, markerstrokewidth=0, clims=lims)
+    U = @. gₗ*cos(2x_U)^2 + Vₗ*cos(x_U + φ)^2
+    plot(x_U ./ π, U, label=false, ylims=lims, c=:white)
+    scatter!(h.w.pos[:, i] ./ π, h.w.E[:, i]; label=false, markerstrokewidth=0, c=1:2n_cells, markersize=5)
     for j in axes(w, 2)
-        plot!(x, abs2.(w[:, j, i]) .+ h.w.E[j, i], label=false)
+        plot!(x ./ π, abs2.(w[:, j, i]) .+ h.w.E[j, i], label=false, c=j)
     end
+    plot!(xlabel=L"x/\pi", ylabel="Energy", title=L"V_{{\rm S}} = %$gₗ, V_{{\rm L}} = %$Vₗ; \varphi_x = %$(round(φ, sigdigits=3))", titlepos=:left)
     next!(p)
 end
 
@@ -66,7 +68,7 @@ end
 
 # Compute Wanniers by mixing the two subbands of `targetband` together. Wanniers are construced at all phases,
 # even though we need them at only one phase when constructing the TB Hamiltonian.
-sm.compute_wanniers!(h, targetband=25, mixsubbands=true)
+sm.compute_wanniers!(h, targetband=27, mixsubbands=true)
 
 # Plot the Wanniers
 x = range(0, n_cells*π, length=50n_cells)
@@ -109,12 +111,12 @@ x = range(start=0, step=π/2, length=2n_cells)
 p = Progress(length(φₓ), 1)
 @gif for (i, iφ) in enumerate(whichphases)
     U = @. gₗ*cos(2x_U)^2 + Vₗ*cos(x_U + φₓ[iφ])^2
-    plot(x_U, U, label=false, ylims=lims, c=:white);
-    scatter!(htb.w.pos[:, iφ], htb.w.E[:, iφ]; label=false, markerstrokewidth=0, ylims=lims, markersize=5, c=1:2n_cells, xlims=(0, π*n_cells))
+    plot(x_U ./ π, U, label=false, ylims=lims, c=:white);
+    scatter!(htb.w.pos[:, iφ] ./ π, htb.w.E[:, iφ]; label=false, markerstrokewidth=0, ylims=lims, markersize=5, c=1:2n_cells, xlims=(0, n_cells))
     for j in axes(wanniers, 2)
-        plot!(x, abs2.(wanniers[:, j, i]) .+ htb.w.E[j, iφ], label=false, c=j)
+        plot!(x ./ π, abs2.(wanniers[:, j, i]) .+ htb.w.E[j, iφ], label=false, c=j)
     end
-    title!(L"\varphi_x = %$(round(φₓ[iφ], sigdigits=3))")
+    plot!(xlabel=L"x/\pi", ylabel="Energy", title=L"V_{{\rm S}} = %$gₗ, V_{{\rm L}} = %$Vₗ; \varphi_x = %$(round(φₓ[iφ], sigdigits=3))", titlepos=:left)
     next!(p)
 end
 
@@ -123,7 +125,7 @@ end
 # φₓ = [range(0, 0.005, length=10); range(0.006, 3.11, length=40); range(3.14, pi, length=10)] # good for the system of Nakajima et al.
 n_cells = 4
 gₗ = -7640; Vₗ = -2
-h = sm.UnperturbedHamiltonian(n_cells; M=1/2, gₗ, Vₗ, φₓ, maxband=30, isperiodic=false)
+h = sm.UnperturbedHamiltonian(n_cells; M=1/2, gₗ, Vₗ, φₓ, maxband=32, isperiodic=false)
 sm.diagonalise!(h)
 
 # Energy spectrum
